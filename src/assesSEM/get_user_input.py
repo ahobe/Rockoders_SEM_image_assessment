d4a686e3137e0d5673f47cc2c2fbd06a07708425
ahobe <alex.hobe@geo.uu.se>
2022-12-05 15:07:59 +0100

    Wrote some tests for asking permission to overwrite files. One fails in a weird way.

--- src/assesSEM/get_user_input.py
+++ src/assesSEM/get_user_input.py
@@ -38,16 +38,21 @@ def deal_with_folder_availability(path):
 
     overwrite_ok = False
     if not empty_folder:
-        answer = input("Folder not empty. Is it ok to overwrite? [Y/n]")
-        if answer == "Y" or answer == "y" or answer == "":  # only enter was pressed, thus choosing the default
-            overwrite_ok = True
-        elif answer == "N" or answer == "n":
-            print('Aborting...')
-            sys.exit()
-        else:
-            print("Unexpected input. Input should be either 'y' or 'n'. Aborting")
-            sys.exit()
+        overwrite_ok = get_ok_for_overwrite()
 
     if overwrite_ok or empty_folder:  # either or both True
         allowed_to_continue = True
         return allowed_to_continue
+
+
+def get_ok_for_overwrite():
+    answer = input("Folder not empty. Is it ok to overwrite? [Y/n]")
+    if answer == "Y" or answer == "y" or answer == "":  # only enter was pressed, thus choosing the default
+        overwrite_ok = True
+    elif answer == "N" or answer == "n":
+        print('Aborting...')
+        sys.exit()
+    else:
+        print("Unexpected input. Input should be either 'y' or 'n'. Aborting")
+        sys.exit()
+    return overwrite_ok
--- tests/test_get_user_input.py
+++ tests/test_get_user_input.py
@@ -1,14 +1,35 @@
 import unittest
-from unittest.mock import Mock, patch
+from io import StringIO
+from unittest.mock import patch
 import pytest
 
-from assesSEM.get_user_input import deal_with_folder_availability, \
+from assesSEM.get_user_input import get_ok_for_overwrite, \
     get_folder_names
 
 
-class TestRun(unittest.TestCase):
-    def test_deal_with_folder_availability(self):
-        self.assertEqual(False, True)
+@pytest.mark.parametrize("response, expected", [
+    ("", True),
+    ("Y", True),
+    ("y", True)
+])
+def test_get_ok_for_overwrite_yes(response, expected):
+    with patch('builtins.input', return_value=response):
+        overwrite_ok = get_ok_for_overwrite()
+        assert overwrite_ok == expected
+
+@pytest.mark.parametrize("response, printed_infos", [
+    ("N", 'Aborting...\n'),
+    ("n", 'Aborting...\n'),
+    ("..5..", "Unexpected input. Input should be either 'y' or 'n'. Aborting")
+])
+def test_get_ok_for_overwrite_no(response, printed_infos):
+    with pytest.raises(SystemExit) as e:
+        with patch('sys.stdout', new = StringIO()) as fake_out:
+            with patch('builtins.input', return_value='N'):
+                get_ok_for_overwrite()
+    assert e.type == SystemExit
+    assert e.value.code is None
+    assert fake_out.getvalue() == printed_infos
 
 
 @pytest.mark.parametrize("response, expected", [
