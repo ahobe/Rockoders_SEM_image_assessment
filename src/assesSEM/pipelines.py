9b895dd80869dd286f737d80a8d67cf5bf8d8b96
ahobe <alex.hobe@geo.uu.se>
2022-12-05 21:13:17 +0100

    Added some ideas on how to improve the overall structure of the pipeline.

--- src/assesSEM/pipelines.py
+++ src/assesSEM/pipelines.py
@@ -14,24 +14,40 @@ from assesSEM.unet import get_unet_input
 
 
 def run_original_pipeline(model_name):
+    # initializing solver
     model, nb_classes, im_h = build_and_load_existing_model(name=model_name)
 
+    # folder structure manipulation
     folder_names = get_folder_names()
     nr_of_images_per_folder = get_desired_nr_of_images_per_folder(folder_names)
     predictions_paths = create_image_predictions_folders(folder_names)
 
     for iFolder, folder in enumerate(folder_names):
+        # pathfinder for all paths (files, folders, folders to be created, files to be created).
+        # class for all name and path types, one instance for each sample
+        #
         path_folder_bse, path_folder_cl = get_names_for_image_type_folders(folder)
         images_in_both = get_common_image_nrs_from_both_image_types(path_folder_bse, path_folder_cl)
 
         # im_dummy = cv2.imread(path_folder_cl + onlyfiles_cl[0], 0)
 
         predictions_path = predictions_paths[iFolder]
+
+        # initialize class again?
         percentage_table = initialize_result_csv(images_in_both)
 
         no_samples = nr_of_images_per_folder[iFolder]
         for iSample in range(no_samples):
+            # only pipeline here
+            # put in all paths for current sample
+            # prep input from images
+            # predict from input
+            # save prediction image yes/no
+            # calculate percentages from prediction image yes/no
+            # save percentages
+
             im_name = images_in_both[iSample]
+            output_file_name = predictions_path + '/' + im_name
             image_path_cl = path_folder_cl + im_name
             image_path_bse = path_folder_bse + im_name
             check1 = os.path.isfile(image_path_cl)
@@ -57,7 +73,6 @@ def run_original_pipeline(model_name):
 
                 # Save &/ plot image
                 test_argmax = np.argmax(predictions_smooth, axis=2)
-                output_file_name = predictions_path + '/' + im_name
 
                 save_image(test_argmax, output_file_name)
 
