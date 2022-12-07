115f71d9b98b4c1fe7713b652a0e27c95a69538f
ahobe <alex.hobe@geo.uu.se>
2022-12-07 22:23:46 +0100

    Made a start on dependecy injection for prediction use case

--- src/assesSEM/use_cases.py
+++ src/assesSEM/use_cases.py
@@ -3,6 +3,26 @@ import time
 from assesSEM.smooth_tiled_predictions import predict_img_with_smooth_windowing
 from assesSEM.unet import get_unet_input
 
+# Use case should not know which solver it is using.
+# It should also not know the specifics of what is required for that solver.
+# The input can also be simpler.
+# If a predictor is chosen, then the required input depends on the chosen predictor.
+#
+def get_input_method_for_predictor_and_model(predictor, model):
+    #if predictor is instance of predict_img_with_smooth_windowing:
+        # input_method = get_unet_input
+    #else:
+        # raise ValueError
+
+    return 0
+
+
+def use_case_predict_from_images(model, images_and_meta, predictor=predict_img_with_smooth_windowing):
+    input_method = get_input_method_for_predictor_and_model(predictor, model)
+    predictor_input = input_method(images_and_meta)
+    predictions = predictor(predictor_input, model)
+    return predictions
+
 
 def predict_from_images(im_h, im_name, image_path_bse, image_path_cl, nb_classes, model):
     print('Segmenting', im_name, '..')
